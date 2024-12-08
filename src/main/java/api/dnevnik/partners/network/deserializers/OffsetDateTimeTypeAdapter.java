bcf4f99e9dac9482b58d4cba54598bd7132f2b6d
alllexey <makarovalesha0212@gmail.com>
2024-12-08 23:56:23 +0800

    Bugfix, 1.2.3

--- src/main/java/api/dnevnik/partners/network/deserializers/OffsetDateTimeTypeAdapter.java
+++ src/main/java/api/dnevnik/partners/network/deserializers/OffsetDateTimeTypeAdapter.java
@@ -2,6 +2,7 @@ package api.dnevnik.partners.network.deserializers;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -11,11 +12,19 @@ public class OffsetDateTimeTypeAdapter extends TypeAdapter<OffsetDateTime> {
 
     @Override
     public void write(JsonWriter out, OffsetDateTime value) throws IOException {
-        out.value(value.toString());
+        if (value == null) {
+            out.nullValue();
+        } else {
+            out.value(value.toString());
+        }
     }
 
     @Override
     public OffsetDateTime read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
         return OffsetDateTime.parse(in.nextString());
     }
 }
